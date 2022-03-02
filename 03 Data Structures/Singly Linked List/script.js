3e9e2a332576636185e6a31b2c5103d72a5fd0bf
David Marom <david.m@radgreen.com>
2022-03-02 09:52:31 +0200

    Progress with linked list

--- 03 Data Structures/Singly Linked List/script.js
+++ 03 Data Structures/Singly Linked List/script.js
@@ -30,17 +30,22 @@ class SinglyLinkedList {
     pop() {
         // find the node before the last node
         let current = this.head;
-
+        let removedNode = null;
         while (current.next.next) {
             current = current.next;
         }
+
+        removedNode = current.next;
         this.tail = current;
         current.next = null;
+        this.length--;
+        return removedNode;
 
 
     }
 
     printList() {
+        console.log("Printing List, length =", this.length);
         let current = this.head;
         while (current.next) {
             console.log(current.val);
@@ -53,14 +58,12 @@ class SinglyLinkedList {
     // End of class 
 }
 
-
-console.log('Start');
 let list = new SinglyLinkedList()
 list.push("1")
 list.push("2")
 list.push("3")
 
 list.printList();
-list.pop();
+console.log(list.pop());
 list.printList();
 
