dd39b0ccf7cdc96f6457b97c915d7b4eec6ef2c3
David Marom <david.m@radgreen.com>
2022-03-08 10:45:48 +0200

    add recursion func

--- 04 Recursion/script.js
+++ 04 Recursion/script.js
@@ -8,4 +8,14 @@ const countDown = (n) => {
     countDown(n);
 }
 
+const factorial = (n) => {
+    if (n === 1) return 1
+    return n * factorial(n - 1);
+}
+
+
+console.log('Count Down:');
 countDown(5);
+console.log('-----------');
+
+console.log('Fatorial 4:', factorial(4));
