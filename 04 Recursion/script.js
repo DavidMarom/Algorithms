bcce8c7f18c0620df85f547337853a4d937f9750
David Marom <david.m@radgreen.com>
2022-03-09 09:31:49 +0200

    power recursion

--- 04 Recursion/script.js
+++ 04 Recursion/script.js
@@ -12,6 +12,11 @@ const factorial = (n) => {
     if (n === 1) return 1
     return n * factorial(n - 1);
 }
+const power = (n, e) => {
+    if (e === 1) return n;
+    return n * power(n, e - 1)
+}
+
 
 
 console.log('Count Down:');
@@ -19,3 +24,8 @@ countDown(5);
 console.log('-----------');
 
 console.log('Fatorial 4:', factorial(4));
+console.log('-----------');
+
+console.log('3 power 5:', power(3, 5));
+console.log('-----------');
+
