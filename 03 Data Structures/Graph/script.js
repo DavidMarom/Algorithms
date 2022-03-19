95b6056fb5c994551a48b6355e075d8f425bbcb7
David Marom <davidmarom@hotmail.com>
2022-03-19 09:32:14 +0200

    graph functions

--- 03 Data Structures/Graph/script.js
+++ 03 Data Structures/Graph/script.js
@@ -6,4 +6,31 @@ class Graph {
     addVertex(vertex) {
         if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
+
+    addEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+
+    }
+
+    removeEdge(v1, v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2)
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1)
+    }
+
+    removeVertex(v1) {
+        while (this.adjacencyList[v1].length) {
+            const adjacentVertex = this.adjacencyList[v1].pop();
+            this.removeEdge(v1,adjacentVertex);
+        }
+        delete this.adjacencyList[v1];
+    }
 }
+let g = new Graph();
+g.addVertex("a");
+g.addVertex("b");
+g.addVertex("c");
+
+g.addEdge("a", "b");
+g.addEdge("a", "c");
+console.log(g.adjacencyList)
